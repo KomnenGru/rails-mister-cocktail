5011e60e0597b378dc293a46ee2cd668c761c1fe
Komnen Grujic <komnengrujic@gmail.com>
2020-11-12 20:22:09 +0100

    Rake green

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,28 @@
+class CocktailsController < ApplicationController
+  before_action :find_index, only: %i[show]
+  def index
+    @cocktails = Cocktail.all
+  end
+  def show
+  end
+  def new
+    @cocktail = Cocktail.new
+  end
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_index
+    @cocktail = Cocktail.find(params[:id])
+  end
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
